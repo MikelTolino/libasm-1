2af101f379088c3c03129fce90beaa56e8cc9583
Leo MARTIN <lmartin@e2r7p22.42.fr>
2019-12-07 20:29:48 +0100

    norme le seul truc a passer a la norme

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 23:28:14 by lmartin           #+#    #+#             */
-/*   Updated: 2019/12/07 16:27:15 by lmartin          ###   ########.fr       */
+/*   Updated: 2019/12/07 20:29:28 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,5 @@ int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t ft_read(int fildes, void *buf, size_t nbyte);
 char	*ft_strdup(const char *s1);
-
 
 #endif
